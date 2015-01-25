ba78aeffe8f20a2b92e512a19434a000f742c6f6
Katie Merry <Katie@katiemecomputer.gateway>
2015-01-25 18:03:35 +1100

    First Draft of R Programming Assignment 2, still working through how to cache the results

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming assignment 2 required us to write a pair of functions that cache the inverse of a matrix.
+## Given Matrix inversion can be costly the instend of these functions is to speed up the process.
+## The two statements below work as follows: makeCacheMatrix creates a matrix and it's inverse, which 
+## can then be called by casheSolve when needed. 
 
-## Write a short comment describing this function
+## This first function creates "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  b <<- matrix(1:4,2,2)
+  solve(b)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the "matrix" returned by makeCacheMatrix above. If the inverse has 
+## already been calculated (and the matrix has not changed), then this function retrieves the inverse 
+## from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  z <<- makematrixprac()
+  if(!is.null(z)) {
+    message("getting cached data")
+    return(z)
+  }
 }
